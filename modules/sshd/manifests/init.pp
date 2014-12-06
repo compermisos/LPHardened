f2922495c1b4737c4e9c70c64892b8f104e9ad40
Jesus Christian Cruz Acono <compermisos@gmail.com>
2014-12-05 21:26:24 -0600

    change the config mode from "pre-generated" to augeas

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -3,17 +3,43 @@ class sshd {
   package { 'openssh-server':
     ensure => installed,
   }
-  file { '/etc/ssh/sshd_config':
-    source  => 'puppet:///modules/sshd/sshd.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+#  file { '/etc/ssh/sshd_config':
+#    source  => 'puppet:///modules/sshd/sshd.conf',
+#    owner   => 'root',
+#    group   => 'root',
+#    mode    => '0644',
+#    require => Package['openssh-server'],
+#    notify  => Service['sshd'],
+#  }
+  augeas { 'sshd_config':
+    context => '/files/etc/ssh/sshd_config',
+    changes => [
+      #Port config
+      'set Port 22',
+      ###Security config###
+      #disable ROOT login
+      'set PermitRootLogin yes',
+      #force Passwords
+      'set PermitEmptyPasswords no',
+      #force Vercion 2
+      'set Protocol 2',
+      #banners
+      'set PrintMotd yes',
+      #issue.net banner
+      'set Banner /etc/issue.net',
+    ],
     require => Package['openssh-server'],
+    notify  => Service['sshd'],
   }
-  service {'ssh':
+  service {'sshd':
+    name => $operatingsystem ? {
+      Debian  => 'ssh',
+      Ubuntu  => 'ssh',
+      default => 'sshd',
+    },
     ensure  => running,
     enable  => true,
-    require => [Package['openssh-server'],File['/etc/ssh/sshd_config']],
+#    require => [Package['openssh-server'],File['/etc/ssh/sshd_config']],
+    require => [Package['openssh-server'],Augeas['sshd_config']],
   }
 }
-
