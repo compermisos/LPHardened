c4be59ae1e589e33838ff192b44523ccdaeab4b6
Jesus Christian Cruz Acono <compermisos@gmail.com>
2015-02-11 12:31:11 -0600

    more files of firewalling #9

---
+++ modules/fw/manifests/init.pp
@@ -0,0 +1,21 @@
+class fw {
+  file { '/etc/rsyslog.d/20-iptables.conf':
+    source  => 'puppet:///modules/fw/syslog.conf',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => file['/var/log/iptables.log'],
+  }#
+  file { '/etc/logrotate.d/iptables':
+    source  => 'puppet:///modules/fw/logrotate.conf',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => file['/etc/rsyslog.d/20-iptables.conf'],
+  }
+  file { '/var/log/iptables.log':
+    owner   => 'syslog',
+    group   => 'adm',
+    mode    => '0640',
+  }
+}
