fc0bc5d5f5505740ae4215fb4dd29414c23a93cd
Jesus Christian Cruz Acono <compermisos@gmail.com>
2015-07-18 16:32:09 -0500

    added securSsH

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -23,10 +23,23 @@ class sshd {
       'set PermitEmptyPasswords no',
       #force Vercion 2
       'set Protocol 2',
+      #user jail
+      'set UsePrivilegeSeparation yes',
+      #permit pubkey auth
+      'set PubkeyAuthentication yes',
+      #permit password login
+      'set PasswordAuthentication no',
+      #challenge response
+      'set ChallengeResponseAuthentication no',
       #banners
       'set PrintMotd yes',
       #issue.net banner
       'set Banner /etc/issue.net',
+      #Secure SSH Server based in https://stribika.github.io/2015/01/04/secure-secure-shell.html
+      'set KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256',
+      'set Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr',
+      'set MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-ripemd160-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,hmac-ripemd160,umac-128@openssh.com',
+      
     ],
     require => Package['openssh-server'],
     notify  => Service['sshd'],
